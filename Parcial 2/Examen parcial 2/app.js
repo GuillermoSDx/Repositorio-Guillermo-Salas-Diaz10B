2cdb1feaca3a6db194d88023fb2074679ecf7a20
GuillermoSDx <memo.leved@gmail.com>
2024-03-14 20:39:40 -0700

    perdon

--- Parcial 2/Examen parcial 2/app.js
+++ Parcial 2/Examen parcial 2/app.js
@@ -41,9 +41,64 @@ function mostrarTodosIds() {
         console.error('Error fetching todos:', error);
       });
   }
+  // Función para mostrar la lista de todos los pendientes resueltos (ID y Title)
+function mostrarTodosResueltos() {
+  fetch('http://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(data => {
+      const resolvedTodosIdsAndTitles = data.filter(todo => todo.completed).map(todo => `${todo.id}: ${todo.title}`);
+      document.getElementById('info').innerText = `Pendientes resueltos (IDs y títulos):\n${resolvedTodosIdsAndTitles.join('\n')}`;
+    })
+    .catch(error => {
+      console.error('Error fetching todos:', error);
+    });
+}
+
+// Función para mostrar la lista de todos los pendientes (IDs y userlD)
+function mostrarTodosIdsAndUserId() {
+  fetch('http://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(data => {
+      const todosIdsAndUserId = data.map(todo => `${todo.id}: ${todo.userId}`);
+      document.getElementById('info').innerText = `Todos los pendientes (IDs y userID):\n${todosIdsAndUserId.join('\n')}`;
+    })
+    .catch(error => {
+      console.error('Error fetching todos:', error);
+    });
+}
+
+// Función para mostrar la lista de los pendientes resueltos (ID Y JASIIP)
+function mostrarPendientesResueltos() {
+  fetch('http://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(data => {
+      const resolvedTodosIdsAndJASIIP = data.filter(todo => todo.completed).map(todo => `${todo.id}: ${todo.userId}`);
+      document.getElementById('info').innerText = `Pendientes resueltos (IDs y JASIIP):\n${resolvedTodosIdsAndJASIIP.join('\n')}`;
+    })
+    .catch(error => {
+      console.error('Error fetching todos:', error);
+    });
+}
+
+// Función para mostrar la lista de todos los pendientes sin resolver (ID y Tittle)
+function mostrarTodosSinResolver() {
+  fetch('http://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(data => {
+      const unresolvedTodosIdsAndTitles = data.filter(todo => !todo.completed).map(todo => `${todo.id}: ${todo.title}`);
+      document.getElementById('info').innerText = `Pendientes sin resolver (IDs y títulos):\n${unresolvedTodosIdsAndTitles.join('\n')}`;
+    })
+    .catch(error => {
+      console.error('Error fetching todos:', error);
+    });
+}
+
   
   // Agregar eventos de clic para las opciones del menú
   document.getElementById('opcion1').addEventListener('click', mostrarTodosIds);
   document.getElementById('opcion2').addEventListener('click', mostrarTodosIdsAndTitles);
   document.getElementById('opcion3').addEventListener('click', mostrarTodosUnresolved);
-  
+  document.getElementById('opcion4').addEventListener('click', mostrarTodosResueltos);
+  document.getElementById('opcion5').addEventListener('click', mostrarTodosIdsAndUserId);
+  document.getElementById('opcion6').addEventListener('click', mostrarPendientesResueltos);
+  document.getElementById('opcion7').addEventListener('click', mostrarTodosSinResolver);
